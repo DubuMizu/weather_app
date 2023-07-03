bde59fef5ef7ea0d4d3cffc72cf8ed87034c1166
DubuMizu <aarontanweiheng@gmail.com>
2023-07-04 00:11:21 +0800

    Added Settings Menu
    Added settings menu with checkboxes to toggle on and off certain attributes like feelsLike temperature, wind direction, etc.
    
    Added a button to go toggle the settings menu.
    
    Added a back button to return to the home page.
    
    Added localStorage to store user data in order to only display the attributes that the user wants.

--- script.js
+++ script.js
@@ -13,11 +13,12 @@ let weather = {
 
         if (response.status == 404){
             document.querySelector(".weather").style.display = "none";
-            document.querySelector(".error").style.display = "block";       
+            document.querySelector(".error").style.display = "block";
         }
 
         else {
             let data = await response.json()
+            console.log(data);
             const {name} = data;
             const {icon, description} = data.weather[0];
             const {temp, humidity} = data.main;
@@ -25,7 +26,7 @@ let weather = {
             document.querySelector(".city").innerHTML = "Weather in " + name;
             document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
             document.querySelector(".description").innerHTML = description;
-            document.querySelector(".temperature").innerHTML = temp.toFixed(1) + "°C";
+            document.querySelector(".temperature").innerHTML = temp.toFixed(1) + "°C" + " | " + (temp * 9/5 + 32).toFixed(1) + "°F";
             document.querySelector(".humidity").innerHTML = "Humidity is " + humidity + "%";
             document.querySelector(".wind").innerHTML = "Wind speed is " + speed +  "km/h";
             document.querySelector(".weather").classList.remove("loading");
@@ -41,6 +42,16 @@ let weather = {
         this.fetchWeather(document.querySelector(".search-bar").value);
     },
 
+    openSettings: function() {
+        document.querySelector(".card").style.display = "none";
+        document.querySelector(".settings").style.display = "block";
+    },
+
+    backSettings: function() {
+        document.querySelector(".card").style.display = "block";
+        document.querySelector(".settings").style.display = "none";
+    }
+
 };
 
 document.querySelector(".search button").addEventListener("click", function() {
@@ -53,4 +64,78 @@ document.querySelector(".search-bar").addEventListener("keyup", function(event) 
     }
 });
 
+document.querySelector(".settings-btn").addEventListener("click", function() {
+    weather.openSettings();
+});
+
+document.querySelector(".btn-back").addEventListener("click", function() {
+    weather.backSettings();
+});
+
+// Local storage
+
+let ls = {
+    defaultSettings: function()
+    {
+        if (localStorage.getItem("isLoaded") === null)
+        {
+            localStorage.setItem("isLoaded", "true");
+            localStorage.setItem("feelsLike", "false");
+            localStorage.setItem("humidity", "true");
+            localStorage.setItem("pressure", "false");
+            localStorage.setItem("minTemp", "false");
+            localStorage.setItem("maxTemp", "false");
+            localStorage.setItem("windSpeed", "true");
+            localStorage.setItem("windDirection", "false");
+        }
+        
+    },
+
+    resetSettings: function()
+    {
+        localStorage.setItem("isLoaded", "true");
+        localStorage.setItem("feelsLike", "false");
+        localStorage.setItem("humidity", "true");
+        localStorage.setItem("pressure", "false");
+        localStorage.setItem("minTemp", "false");
+        localStorage.setItem("maxTemp", "false");
+        localStorage.setItem("windSpeed", "true");
+        localStorage.setItem("windDirection", "false");
+    },
+
+    checkSettings: function()
+    {
+        if (localStorage.getItem("feelsLike") == "true")
+        {
+            document.querySelector(".feelslikeBox").checked = true;
+        }
+        if (localStorage.getItem("humidity") == "true")
+        {
+            document.querySelector(".humidityBox").checked = true;
+        }
+        if (localStorage.getItem("pressure") == "true")
+        {
+            document.querySelector(".pressureBox").checked = true;
+        }
+        if (localStorage.getItem("minTemp") == "true")
+        {
+            document.querySelector(".mintempBox").checked = true;
+        }
+        if (localStorage.getItem("maxTemp") == "true")
+        {
+            document.querySelector(".maxtempBox").checked = true;
+        }
+        if (localStorage.getItem("windSpeed") == "true")
+        {
+            document.querySelector(".windspeedBox").checked = true;
+        }
+        if (localStorage.getItem("windDirection") == "true")
+        {
+            document.querySelector(".winddirectionBox").checked = true;
+        }
+    },
+}
+
+ls.defaultSettings();
+ls.checkSettings();
 
